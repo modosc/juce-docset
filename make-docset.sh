1e7deee3ed9c09bdd4070e4b77f699f2a164d85f
jonathan schatz <jon@divisionbyzero.com>
2018-03-15 14:09:14 -0700

    update juce to 5.3.0, update build script

--- make-docset.sh
+++ make-docset.sh
@@ -2,18 +2,23 @@
 
 git submodule update --init
 
+rm -rf JUCE.docset
+rm -rf JUCE.tgz
+
 cd JUCE/doxygen
+make clean
 sed -i.bak \
   -e 's/.*GENERATE_DOCSET.*= NO.*/GENERATE_DOCSET = YES/' \
   -e 's/.*DISABLE_INDEX.*= NO.*/DISABLE_INDEX = YES/' \
   -e 's/.*SEARCHENGINE.*= YES.*/SEARCHENGINE = NO/' \
   -e 's/.*GENERATE_TREEVIEW.*= YES.*/GENERATE_TREEVIEW = NO/' \
   -e 's/.*HTML_HEADER.*=.*/HTML_HEADER = ..\/..\/header.html/' \
+  -e 's/.*DOCSET_BUNDLE_ID.*=.*/DOCSET_BUNDLE_ID = JUCE/' \
   Doxyfile
-doxygen
+make
 cd doc
 make
-cp -r org.doxygen.Project.docset ../../../JUCE.docset
+cp -r JUCE.docset ../../../
 cd ../../..
 
 iconPath=JUCE/extras/Projucer/Source/BinaryData/juce_icon.png
